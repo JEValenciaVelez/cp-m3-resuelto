2a546dda84d73edaafbb0ac4faba43676e89e5f7
Juan Esteban <juancho50301@gmail.com>
2023-04-16 20:34:56 -0500

    'actualizacion'

--- routes/13-route.js
+++ routes/13-route.js
@@ -5,7 +5,14 @@ const controller = require('../controllers/controllers');
 
 // Escribe la lógica de las rutas acá
 router.put("/accessories/update-popularity", (req, res) => {
+
+  const {accessoryId} = req.body;
    
+  try{
+    res.status(200).send( controller.updateAccessoryPopularity(accessoryId));
+  }catch(error){
+    res.status(404).json({error: error.message});
+  }
   });
 
 // No modifiques nada debajo de esta línea
